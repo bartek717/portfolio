91ee34cd607644f1f4bae90d502aae237456cac5
bartek717 <bartekkowalski925@gmail.com>
2023-11-25 21:38:35 -0500

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -9,31 +9,35 @@ export default function Home() {
       <div className={styles.contentWrapper}>
         <div className={styles.mainContainer}>
           <div className={styles.textSection}>
-            <h1 className={styles.header}>Bartek Kowalski</h1>
-            <p className={styles.mainText}>I'm a dedicated Founder, Machine Learning Developer,
-               Student out of Queens University.</p>
-            <p className={styles.mainText}>My interests ly in the fields of NLP, Reinforcement
-               Learning and Autonomous Agents.</p>
+            <div>
+              <h1 className={styles.header}>Bartek Kowalski</h1>
+              <p className={styles.mainText}>I'm a dedicated Founder, Machine Learning Developer,
+                 Student out of Queens University.</p>
+              <p className={styles.mainText}>My interests ly in the fields of NLP, Reinforcement
+                 Learning and Autonomous Agents.</p>
+            </div>
             <div className={styles.socialLinks}>
-              <a href="https://github.com/bartek717" target="_blank" rel="noopener noreferrer">
+              <a className={styles.link} href="https://github.com/bartek717" target="_blank" rel="noopener noreferrer">
                 <Image src='/github.png' width={33} height={33} alt='GitHub' />
               </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
+              <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">
                 <Image src='/linkedin.png' width={33} height={33} alt='LinkedIn' />
               </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
+              <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">
                 <Image src='/medium.png' width={33} height={33} alt='Medium' />
               </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
+              <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">
                 <Image src='/twitter.png' width={33} height={33} alt='Twitter' />
               </a>
-              <a href="mailto:bartekkowalski465@gmail.com">
+              <a className={styles.link} href="mailto:bartekkowalski465@gmail.com">
                 <Image src='/email.png' width={33} height={25} alt='Email' />
               </a>
             </div>
           </div>
           <div className={styles.imageSection}>
-            <Image src='/meinqubateheadshots.jpg' width={608} height={778} alt='image of Bartek Kowalski'></Image>
+            <div className={styles.picture}>
+              <Image src='/meinqubateheadshots.jpg' width={608} height={778} alt='image of Bartek Kowalski'></Image>
+            </div>
           </div>
         </div>
       </div>
