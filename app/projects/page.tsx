89c3de52944c8d7dd8af94126ba64f48db32b206
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-11-26 12:31:27 -0500

    stash

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,8 +8,28 @@ function page() {
       <Header />
       <div className={styles.contentWrapper}>
         <div className={styles.mainContainer}>
-        
-        hi!
+          <div className={styles.textSection}>
+            <h1 className={styles.header}>
+              Projects
+            </h1>
+            <p className={styles.mainText}>
+              I've done a variety of projects, spanning from machine learning models to full stack web apps.
+            </p>
+            <p className={styles.mainText}>
+              Here you can see them all with (some) github links. 
+            </p>
+          </div>
+          <div className={styles.contentSection}>
+            <div>
+              project1
+            </div>
+            <div>
+              project2
+            </div>
+            <div>
+              project3
+            </div>
+          </div>
         </div>
       </div>
     </div>
