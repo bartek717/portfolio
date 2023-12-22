4f2cf67498662a16852d3712d4fdbde1feb9635f
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-12-22 17:39:31 -0500

    rewrite homepage header and formatting

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from "./Header.module.css"
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import styles from "./Header.module.css";
 
-function header() {
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        {/* Hamburger icon */}
+        <span>Menu</span>
+      </div>
+
+      <div className={`${styles.mobileLinks} ${isMenuOpen ? styles.open : ''}`}>
+        <button className={styles.closeButton} onClick={toggleMenu}>Close</button>
+        <Link href='/' onClick={toggleMenu}>Home</Link>
+        <Link href='/projects' onClick={toggleMenu}>Projects</Link>
+        <Link href='/research' onClick={toggleMenu}>Research</Link>
+        <Link href='/articles' onClick={toggleMenu}>Articles</Link>  
+        <Link href='/experience' onClick={toggleMenu}>Experience</Link>
+        {/* <Link href='/mindmap' onClick={toggleMenu}>Mindmap</Link> */}
+        <Link href='/contact' onClick={toggleMenu}>Contact</Link>
+      </div>
+
       <div className={styles.links}>
-   
-        <Link className='px-4' href='/'>Home</Link>
+        <Link href='/'>Home</Link>
         <Link href='/projects'>Projects</Link>
         <Link href='/research'>Research</Link>
         <Link href='/articles'>Articles</Link>  
         <Link href='/experience'>Experience</Link>
         {/* <Link href='/mindmap'>Mindmap</Link> */}
         <Link href='/contact'>Contact</Link>
-        
-      
       </div>
     </header>
-  )
+  );
 }
 
-export default header
+export default Header;
+
