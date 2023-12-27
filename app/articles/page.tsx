c0b14c479a56dabd701072121cfa0dba2f470451
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-12-26 22:33:38 -0500

    reorder

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -27,16 +27,16 @@ function page() {
           <div className={styles.contentSection}>
             <div>
               <Project 
-              title="ChatGPT: Is the Education Industry Ready?" 
-              description="Over the past year or so I've seen the education landscape completely transform. Its impact on the next generation of students will be profound, and limit their creativity and problem solving. What can we do about this?" 
-              mediumLink='https://medium.com/qmind-ai/chatgpt-is-the-education-industry-ready-df8bd7575371'
-              image='school.png'
-              />
-              <Project 
                 title="The Rise and Potential of Open Source Large Language Models in AI" 
                 description="Over the past couple of months, we have seen the emergence of Large Language models that begin to compete with the state of the art. Where does this go next?" 
                 mediumLink='https://medium.com/@bartekmkowalski/the-rise-and-potential-of-open-source-large-language-models-in-ai-0d30450f8b3c'
                 image='brain2.png'
+              />
+              <Project 
+              title="ChatGPT: Is the Education Industry Ready?" 
+              description="Over the past year or so I've seen the education landscape completely transform. Its impact on the next generation of students will be profound, and limit their creativity and problem solving. What can we do about this?" 
+              mediumLink='https://medium.com/qmind-ai/chatgpt-is-the-education-industry-ready-df8bd7575371'
+              image='school.png'
               />
             </div>
             
